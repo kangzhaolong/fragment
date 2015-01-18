2a6664b41646a64d02b5ab5a4ccb61f4814166df
zhaolong <kangzhaolong@gmail.com>
2015-01-18 11:46:02 +0800

    init
    Init project

---
+++ fragment/src/main/java/com/kalong/fragment/JMainApp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kalong.fragment;
+
+/**
+ *
+ * @author kalong
+ */
+public class JMainApp {
+    public static void main(String[] agv)
+    {
+        System.out.println("Hello world");
+    }
+}
